5ba4032614e3e26cb531ff874488998d22c8ddc8
unknown <nasa@gmail.com>
2026-04-05 10:50:31 +0530

    Fix whitenoise storage error

--- livesofchrist/settings.py
+++ livesofchrist/settings.py
@@ -136,4 +136,4 @@ REST_FRAMEWORK = {
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.StaticFilesStorage'
